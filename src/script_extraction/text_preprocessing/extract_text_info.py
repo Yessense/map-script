d7d8018611ffe1dcf0f3022237e464ca40ae1aee
yessense <Akorchemnyj@gmail.com>
2021-07-02 12:43:31 +0300

    parsing for one text is working

--- src/script_extraction/text_preprocessing/extract_text_info.py
+++ src/script_extraction/text_preprocessing/extract_text_info.py
@@ -2,8 +2,11 @@ from allennlp.predictors.predictor import Predictor
 import allennlp_models.tagging
 import nltk.data
 
+file_name = "/home/yessense/PycharmProjects/ScriptExtractionForVQA/texts/restaurant.txt"
+
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-with open("/home/yessense/PycharmProjects/ScriptExtractionForVQA/texts/restaurant.txt") as f:
+
+with open(file_name) as f:
     text = f.read()
 
 tokenized_text = tokenizer.tokenize(text)
@@ -11,3 +14,16 @@ tokenized_text = tokenizer.tokenize(text)
 open_information_extractor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz")
 dependency_parser = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz")
 semantic_role_labeler = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/structured-prediction-srl-bert.2020.12.15.tar.gz")
+
+predictors = {"open information": open_information_extractor,
+              "dependancy": dependency_parser,
+              "semantic role": semantic_role_labeler}
+text_info = {}
+for sentence_index, sentence in enumerate(tokenized_text):
+    sentence_info = {}
+    for predictor_name, predictor in predictors.items():
+        parsed_data = predictor.predict(sentence)
+        sentence_info[predictor_name] = parsed_data
+    text_info[sentence_index] = sentence_info
+
+print("Done")
