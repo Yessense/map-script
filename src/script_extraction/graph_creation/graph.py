a7d6c7120ad3aa0cf9147a2b47fbbe2be6d12337
yessense <Akorchemnyj@gmail.com>
2021-07-03 14:40:01 +0300

    add module tree, graph

---
+++ src/script_extraction/graph_creation/graph.py
@@ -0,0 +1,17 @@
+class graph:
+    def __init__(self, texts_info):
+        pass
+
+    def get_tree(self, text_info):
+        """
+
+        Parameters
+        ----------
+        text_info
+
+        Returns
+        -------
+        out: Tree
+            verb Tree
+        """
+
