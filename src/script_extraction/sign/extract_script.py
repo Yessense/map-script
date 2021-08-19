d48418d0674d68ea5d4dfdb438e6285c10fc5a88
Yessense <Akorchemnyj@gmail.com>
2021-08-19 10:43:12 +0300

    delete unused files

--- src/script_extraction/sign/extract_script.py
+++ src/script_extraction/sign/extract_script.py
@@ -1,5 +1,5 @@
 import enum
-from typing import Any, Dict, List, Union, Iterable
+from typing import Any, Dict, List, Union, Iterable, Optional
 
 from mapcore.swm.src.components.semnet import Sign, Event, CausalMatrix, Connector
 
@@ -45,22 +45,40 @@ def add_new_sign(script: Dict[str, Sign],
 def add_role_object_to_action(script: Dict[str, Sign],
                               roles_dict: Dict[Roles, int],
                               action: Action,
-                              role_object: Obj) -> None:
+                              role_object: Obj,
+                              arg_type: Optional[Roles] = None) -> None:
     if role_object.synsets_len == -1:
         return
+    if arg_type is None:
+        arg_type = role_object.arg_type
     action_sign = script[action.lemma]
     role_sign = script[role_object.lemma]
     action_cm: CausalMatrix = action_sign.significances[action.synset_number + 1]
     # action_cm.add_feature(script[role_object.lemma].significances[role_object.synset_number + 1])
-    action_event: Event = action_cm.cause[roles_dict[role_object.arg_type]]
+    action_event: Event = action_cm.cause[roles_dict[arg_type]]
     connector: Connector = Connector(in_sign=action_sign,
                                      out_sign=role_sign,
                                      in_index=action.synset_number + 1,  # action signifincance number
                                      out_index=role_object.synset_number + 1,
                                      # role significance number
                                      in_order=roles_dict[
-                                         role_object.arg_type])  # role number (Event number)
+                                         arg_type])  # role number (Event number)
     action_event.add_coincident(base='significance', connector=connector)
+
+def add_image_to_object(script: Dict[str, Sign],
+                        obj: Obj,
+                        image: WordsObject):
+    if image.synsets_len != -1:
+        obj_image_matrix: CausalMatrix = script[obj.lemma].images[obj.synset_number + 1]
+        obj_image_event: Event = obj_image_matrix.cause[0]
+
+        # connector to image
+        connector: Connector = Connector(in_sign=script[obj.lemma],
+                                         out_sign=script[image.lemma],
+                                         in_index=obj.synset_number + 1,  # obj images number
+                                         out_index=image.synset_number + 1,  # image images number
+                                         in_order=0)
+        obj_image_event.add_coincident(base='image', connector=connector)
 
 
 def extract_script(text_info: Dict[str, Any]):
@@ -89,26 +107,25 @@ def extract_script(text_info: Dict[str, Any]):
         if len(action.objects):
             action_sign = script[action.lemma]
             for role_object in action.objects:
-                add_role_object_to_action(script, roles_dict, action, role_object)
-                # if role_object.cluster is not None:
-                #     for
+                if role_object.cluster is None:
+                    add_role_object_to_action(script, roles_dict, action, role_object)
+                else:
+                    for role_cluster_object in role_object.cluster.real_objects:
+                        add_role_object_to_action(script, roles_dict, action, role_cluster_object,
+                                                  arg_type=role_object.arg_type)
 
     # add images
     for action in actions:
         for obj in action.objects:
-            if obj.synsets_len != -1:
+            if obj.synsets_len == -1:
+                continue
+            if obj.cluster is None:
                 for image in obj.images:
-                    if image.synsets_len != -1:
-                        obj_image_matrix: CausalMatrix = script[obj.lemma].images[obj.synset_number + 1]
-                        obj_image_event: Event = obj_image_matrix.cause[0]
+                    add_image_to_object(script, obj, image)
+            else:
+                for image in obj.cluster.images.values():
+                    add_image_to_object(script, obj, image)
 
-                        # connector to image
-                        connector: Connector = Connector(in_sign=script[obj.lemma],
-                                                         out_sign=script[image.lemma],
-                                                         in_index=obj.synset_number + 1,  # obj images number
-                                                         out_index=image.synset_number + 1,  # image images number
-                                                         in_order=0)
-                        obj_image_event.add_coincident(base='image', connector=connector)
     return script
 
 
