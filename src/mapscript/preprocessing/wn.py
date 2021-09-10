49cdb6e6f611e09c857a866319b11ea560c62b18
yessense <Akorchemnyj@gmail.com>
2021-09-10 18:19:17 +0300

    Lemma check for synsets

--- src/mapscript/preprocessing/wn.py
+++ src/mapscript/preprocessing/wn.py
@@ -8,7 +8,7 @@ from nltk.wsd import lesk
 
 
 def get_meaning(sentence: List[str], lemma: str, pos: str) -> Tuple[int, int]:
-    synsets: List[Synset] = wn.synsets(lemma)
+    synsets: List[Synset] = [l for l in wn.synsets(lemma) if l.lemma == lemma]
 
     # word not represented in dict
     if not len(synsets):
@@ -47,10 +47,10 @@ def example_usage():
             'and', 'funny', '.']
 
     # stack overflow
-    for i, j in enumerate(wn.synsets('family')):
+    for i, j in enumerate(wn.synsets('man')):
         print('Meaning', i, 'NLTK ID', j.name())
         print('Definition:', j.definition())
-        print('Hypernyms:', ', '.join(list(chain(*[l.lemma_names() for l in j.hypernyms()]))))
+        print('Hypernyms:', ', '.join(list(chain(*[l.lemma_names() for l in j.hyponyms()]))))
 
     ss = lesk(sent, 'family')
 
