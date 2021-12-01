deb0a4f952a8129e0efe3da69b00d778b3f94313
yessense <Akorchemnyj@gmail.com>
2021-12-01 10:04:13 +0300

    Pass this

--- src/mapscript/script.py
+++ src/mapscript/script.py
@@ -169,7 +169,7 @@ class Script:
                 else:
                     for role_cluster_object in role_object.cluster.objects:
                         if role_cluster_object.lemma not in self.objects_signs:
-                            self._add_object_sign(role_cluster_object)
+                            continue
                         self._add_role_object_to_action(action_sign=action_sign,
                                                         action=action,
                                                         role_sign=self.objects_signs[role_cluster_object.lemma],
